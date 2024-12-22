ea30dd5f919122786f306f11fff0c90fc78fefc4
mskian <10300271+mskian@users.noreply.github.com>
2024-12-22 15:55:37 +0530

    improve static routes and error handler

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,26 @@
-import express from "express";
+import express, { Application, Request, Response } from 'express';
+import path from 'path';
 import router from "./routes";
+import errorHandler from './utils/errorHandler';
 
-const app = express();
+const app: Application = express();
 const PORT = process.env.PORT || 6026;
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 app.use("/api", router);
 
-app.use('/', function(req, res) {
-  res.status(404).json({
-      error: 1,
-      message: 'Page not Found'
-  });
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ error: 'Resource not found' });
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
